f4f33935f1a08638e9f0d21c827256ae18363d5f
G-Audige <gloriaaudige@gmail.com>
2023-11-30 09:32:48 -0500

    Push

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import './findATheatre.css';
+import './App.css'
 import FindATheatre from './components/FindATheatrePage';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 function App() {
   // const apiKey = '44FAF92B-1A86-46AB-93C4-1475F927B05A';
-  // const url = ` 	https://api.sandbox.amctheatres.com/`;
-  // const [market, setMarket] = useState('null');
-  // const getMarket = async () => {
-  //   const response = await fetch(url);
-  //   const data = await response.json();
-  //   setMarket(data);
-  //   console.log(market);
-  // };
-  // getMarket();
-  // fetch('https://jsonplaceholder.typicode.com/comments')
-  //   .then((response) => response.json())
-  //   .then((json) => console.log(json));
+
   return (
     <div className='App'>
       <FindATheatre />
--- src/components/FindATheatrePage.jsx
+++ src/components/FindATheatrePage.jsx
@@ -3,16 +3,21 @@ import React from 'react';
 import Markets from '../pages/Markets';
 import States from '../pages/States';
 import FindATheatreNav from './FindATheatreNav';
+import Footer from './Footer';
+import Footerlogo from './Footerlogo';
 
 function FindATheatre() {
   return (
-    <div className='find-a-theatre'>
-      <div className='content'>
+    <div className="find-a-theatre">
+      <div className="content">
         <FindATheatreNav />
         <Routes>
-          <Route path='/markets' element={<Markets />} />
-          <Route path='/states' element={<States />} />
+          <Route path="/markets" element={<Markets />} />
+          <Route path="/states" element={<States />} />
         </Routes>
+        <img src="./links.png" className="img" alt="" />
+        <Footer />
+        <Footerlogo />
       </div>
     </div>
   );
